bd64e7e40c4e5506f8c9b7d0545b4333600438ea
Guilherme-Jardim <guilhermejardimj@gmail.com>
2024-05-21 21:14:25 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box className="p-5 m-1">
+      <Box className="flex flex-row">
+        <h1>teste</h1>
+      </Box>
+    </Box>
+  );
+}
