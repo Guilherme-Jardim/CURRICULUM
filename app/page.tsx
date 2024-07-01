98525c7879d5f676f9311567b0f7f97f1d6c8135
guilherme-jardim <guilhermejardimj@gmail.com>
2024-07-01 20:14:46 -0300

    english curriculum

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,13 @@ export default function Curriculum() {
           </Box>
           <Box className="border-t border-black flex items-center mb-4 hover:underline pt-4">
             <TouchApp className="mr-2" />
-            <Typography className="font-bold">
-              Acesse Links e Certificados ao clicar ou tocar.
+            <Typography className="font-bold">              
+              Access Links and Certificates by clicking or tapping.
             </Typography>
           </Box>          
-          <Typography variant="h6" className="border-b border-black mb-4">DESCUBRA MINHAS HABILIDADES</Typography>
+          <Typography variant="h6" className="border-b border-black mb-4">
+            DISCOVER MY SKILLS
+          </Typography>
           <Link href="https://github.com/Guilherme-Jardim" target="_blank" className="flex items-center mb-4 hover:underline">
             <GitHub className="mr-2" />
             <Typography className="font-bold">GITHUB</Typography>
@@ -43,44 +45,54 @@ export default function Curriculum() {
           </Link>
           <Typography variant="h6" className="border-b border-black mb-4">PERFIL</Typography>
           <Typography className="mb-4 indent-2.5">
-            Sou uma pessoa bem-humorada, sociável, comunicativa e criativa. Procuro por novos desafios constantemente e principalmente aprimorar todos os conhecimentos em geral que possuo, me atualizando as futuras novidades e adaptações.
+            I am a good-natured, sociable, communicative and creative person. I constantly look for new challenges and mainly improve all the general knowledge I have, updating myself on future news and adaptations.
           </Typography>
-          <Typography variant="h6" className="border-b border-black mb-4">EXPERIÊNCIAS NO TRABALHO</Typography>
-            <Typography className="mb-4 indent-2.5">
-              Integrações com prefeituras para emissão de RPS, emissão de documentos fiscais e não fiscais como NF-e, NFC-e e PDV.
-            </Typography>
-            <Typography className="mb-4 indent-2.5">
-              Desenvolvimento de projetos como o sistema de Cashback ERP.
-            </Typography>  
-            <Typography className="mb-4 indent-2.5">
-              Estruturação de banco de dados.
-            </Typography> 
-            <Typography className="mb-4 indent-2.5">
-              Manutenção e desenvolvimento de novas funcionalidades em sistema ERP.
-            </Typography>               
-          <Typography variant="h6" className="border-b border-black mb-4">OBJETIVO</Typography>
+          <Typography variant="h6" className="border-b border-black mb-4">
+            EXPERIENCES AT WORK
+          </Typography>
           <Typography className="mb-4 indent-2.5">
-            Atualmente, sou desenvolvedor Delphi e aprimorei minhas habilidades em desenvolvimento de software. Agora, estou embarcando em uma jornada de aprendizado em JavaScript, estudando bibliotecas e frameworks utilizados na atualidade. Continuo aperfeiçoando meus conhecimentos constantemente, e meu objetivo prioritário é começar a trabalhar com linguagem Web.
+            Integrations with city halls to issue RPS, issue fiscal and non-fiscal documents such as NF-e, NFC-e and PDV.
+          </Typography>
+          <Typography className="mb-4 indent-2.5">              
+            Development of projects such as the Cashback ERP system.
+          </Typography>  
+          <Typography className="mb-4 indent-2.5">             
+            Database structuring. 
+          </Typography> 
+          <Typography className="mb-4 indent-2.5">              
+            Maintenance and development of new features in the ERP system.
+          </Typography>               
+          <Typography variant="h6" className="border-b border-black mb-4">OBJECTIVE</Typography>
+          <Typography className="mb-4 indent-2.5">
+            I am currently a Delphi developer with knowledge in software development. I have honed my skills throughout my journey as a developer and am now embarking on a path to learn JavaScript, focusing on modern libraries and frameworks. I am continually enhancing my knowledge and skills, with the primary goal of transitioning to web development.
           </Typography>
           <Box className="flex flex-col items-stretch" >
-            <Typography variant="h6" className="border-b border-black mb-4">IDIOMAS</Typography>
+            <Typography variant="h6" className="border-b border-black mb-4">
+              LANGUAGES
+            </Typography>
             <Tooltip title="75%">
-              <Typography className="mb-2">Espanhol</Typography>
+              <Typography className="mb-2">
+                Spanish
+              </Typography>
             </Tooltip>
             <LinearProgress variant="determinate" value={75} className="h-2 rounded-lg mb-4"/>
             <Tooltip title="60%">
-              <Typography className="mb-2">Inglês</Typography>
+              <Typography className="mb-2">
+                English
+              </Typography>
             </Tooltip>
             <LinearProgress variant="determinate" value={60} className="h-2 rounded-lg mb-4"/>
           </Box>
-          <Typography variant="h6" className="border-b border-black mb-4">CONTATO</Typography>
+          <Typography variant="h6" className="border-b border-black mb-4">
+            CONTACT
+          </Typography>
           <Link href="tel:+54991411153" className="flex items-center mb-4 hover:underline">
             <WhatsApp className="mr-2" />
-            (54) 9 9141-1153
+            +55 (54) 9 9141-1153
           </Link>
           <Link href="tel:+54991411153" className="flex items-center mb-4 hover:underline">
             <Phone className="mr-2" />
-            (54) 9 9141-1153
+            +55 (54) 9 9141-1153
           </Link>
           <Link href="mailto:guilhermejardimj@gmail.com" className="flex items-center mb-4 hover:underline">
             <Email className="mr-2" />
@@ -90,79 +102,125 @@ export default function Curriculum() {
         <Box className="w-1/10 bg-gray-100 rounded-lg p-6">
           <Typography variant="h3" className="font-bold mb-1">Guilherme Jardim</Typography>
           <Box>
-            <Typography variant="h6" className="border-b border-black mb-4">GRADUAÇÃO</Typography>
+            <Typography variant="h6" className="border-b border-black mb-4">
+              BACHELOR DEGREE
+            </Typography>
             <Box className="mb-8">
-              <Typography variant="h6" fontWeight="bold">Ciência da Computação - Graduação - Descomplica (EAD)</Typography>
-              <Typography>2024 - conclusão em 2027</Typography>
+              <Typography variant="h6" fontWeight="bold">
+                Computer Science - Descomplica Digital College
+              </Typography>
+              <Typography>
+                2024 - conclusion in 2027
+              </Typography>
               <Divider />
             </Box>
             <Box className="mb-8">
-              <Typography variant="h6" fontWeight="bold">Ciência da Computação - Graduação - Universidade de Caxias do Sul - UCS</Typography>
-              <Typography>2022 - 2024 (Transferência)</Typography>
+              <Typography variant="h6" fontWeight="bold">
+                Computer Science - University of Caxias do Sul - UCS
+              </Typography>
+              <Typography>
+                2022 - 2024 (Transfer to Descomplica Digital College)
+              </Typography>
               <Divider />
             </Box>
           </Box>
           <Box>
-            <Typography variant="h6" className="border-b border-black mb-4">CURSOS E CERTIFICAÇÕES</Typography>
+            <Typography variant="h6" className="border-b border-black mb-4">
+              COURSES AND CERTIFICATIONS
+            </Typography>
             <Box className="mb-8">
               <Link href="https://app.rocketseat.com.br/me/guilherme-jardim-03544" target="_blank" className="hover:underline">
-                <Typography variant="h6" fontWeight="bold">Formação Node - Rocketseat</Typography>
-                <Typography>Em andamento - 60% | 50 horas</Typography>
+                <Typography variant="h6" fontWeight="bold">
+                  Node - Rocketseat
+                </Typography>
+                <Typography>
+                  In progress - 60% | 50 hours
+                </Typography>
               </Link>              
               <Divider />
             </Box>
             <Box className="mb-8">
               <Link href="https://app.rocketseat.com.br/me/guilherme-jardim-03544" target="_blank" className="hover:underline">
-                <Typography variant="h6" fontWeight="bold">Formação React - Rocketseat</Typography>
-                <Typography>Em andamento - 30% | 50 horas</Typography>
+                <Typography variant="h6" fontWeight="bold">
+                  React - Rocketseat
+                </Typography>
+                <Typography>
+                  In progress - 30% | 50 hours
+                </Typography>
               </Link>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Link href="https://www.origamid.com/certificate/733b17d8" target="_blank" className="hover:underline">
-                <Typography variant="h6" fontWeight="bold">TypeScript para Iniciantes - Origamid</Typography>
-                <Typography>Concluído em 2023 - 22 horas</Typography>
+                <Typography variant="h6" fontWeight="bold">
+                  TypeScript - Origamid
+                </Typography>
+                <Typography>
+                  Completed in 2023 - 22 hours
+                </Typography>
               </Link>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Link href="https://www.origamid.com/certificate/ba0d3359" target="_blank" className="hover:underline">
-                <Typography variant="h6" fontWeight="bold">React Completo - Origamid</Typography>
-                <Typography>Concluído em 2023 - 36 horas</Typography>
+                <Typography variant="h6" fontWeight="bold">
+                  React - Origamid
+                </Typography>
+                <Typography>
+                  Completed in 2023 - 36 hours
+                </Typography>
               </Link>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Link href="https://www.origamid.com/certificate/f47637d3" target="_blank" className="hover:underline">
-                <Typography variant="h6" fontWeight="bold">JavaScript Completo ES6 - Origamid</Typography>
-                <Typography>Concluído em 2023 - 74 horas</Typography>
+                <Typography variant="h6" fontWeight="bold">
+                  JavaScript ES6 - Origamid
+                </Typography>
+                <Typography>
+                  Completed in 2023 - 74 hours
+                </Typography>
               </Link>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Link href="https://www.origamid.com/certificate/3a5b0a16" target="_blank" className="hover:underline">
-                <Typography variant="h6" fontWeight="bold">UI Design para Iniciantes - Origamid</Typography>
-                <Typography>Concluído em 2023 - 34 horas</Typography>
+                <Typography variant="h6" fontWeight="bold">
+                  UI | UX Design - Origamid
+                </Typography>
+                <Typography>
+                  Completed in 2023 - 34 hours
+                </Typography>
               </Link>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Link href="https://www.origamid.com/certificate/c4e36c83" target="_blank" className="hover:underline">
-                <Typography variant="h6" fontWeight="bold">HTML e CSS para Iniciantes - Origamid</Typography>
-                <Typography>Concluído em 2022 - 46 horas</Typography>
+                <Typography variant="h6" fontWeight="bold">
+                  HTML5 | CSS3 - Origamid
+                </Typography>
+                <Typography>
+                  Completed in 2022 - 46 hours
+                </Typography>
               </Link>
               <Divider />
             </Box>
             <Box className="mb-8">
-              <Typography variant="h6" fontWeight="bold">PHP Formação - FLEXXO</Typography>
-              <Typography>Concluído em 2012 - 120 horas</Typography>
+              <Typography variant="h6" fontWeight="bold">
+                PHP - FLEXXO
+              </Typography>
+              <Typography>
+                Completed in 2012 - 120 hours
+              </Typography>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Typography variant="h6" fontWeight="bold">
-                Manutenção de Computadores: Hardware-MS-DOS-Windows/rede-Windows NT4.0/Rede-Windows 2000/Rede-Linux - Amiga Informática
+                Computer maintenance: Hardware-MS-DOS-Windows/Network-Windows NT4.0/Network-Windows2000/Network-Linux
               </Typography>
-              <Typography>Concluído em 2008 - 200 horas</Typography>
+              <Typography>
+                Completed in 2008 - 200 hours
+              </Typography>
               <Divider />
             </Box>
           </Box>
@@ -170,22 +228,28 @@ export default function Curriculum() {
             <Typography variant="h6" className="border-b border-black mb-4">EXPERIÊNCIA DE TRABALHO</Typography>
             <Box className="mb-8">
               <Typography variant="h6" fontWeight="bold">Windel Sistemas</Typography>
-              <Typography>Desenvolvedor de Software - dez/2023 - 2024 (Trabalho atual)</Typography>
+              <Typography>
+                Software Developer - Dec/2023 - 2024 (Current job)
+              </Typography>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Typography variant="h6" fontWeight="bold">Windel Sistemas</Typography>
-              <Typography>Experiência como Tester - set/2023 - dez/2023 (3 meses)</Typography>
+              <Typography>Software Tester - Sep/2023 - Dec/2023 (3 months)</Typography>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Typography variant="h6" fontWeight="bold">Windel Sistemas</Typography>
-              <Typography>Suporte Técnico - dez/2022 - set/2023 (9 meses)</Typography>
+              <Typography>
+                Technical Support - Dec/2022 - Sep/2023 (9 months)
+              </Typography>
               <Divider />
             </Box>
             <Box className="mb-8">
               <Typography variant="h6" fontWeight="bold">Wopus</Typography>
-              <Typography>Estágio Desenvolvedor Front-End - 2022 (1 mês)</Typography>
+              <Typography>
+                Front-End Developer Internship - 2022 (1 month)
+              </Typography>
               <Divider />
             </Box>
           </Box>
